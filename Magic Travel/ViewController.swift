70f935a7066d8f21884c44c0567fa8a8c833aa3c
hanifan <hanifannurulhaq@outlook.com>
2024-02-07 20:40:30 +0700

    add table view data source

--- Magic Travel/ViewController.swift
+++ Magic Travel/ViewController.swift
@@ -19,3 +19,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, 
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return tableViewCell
+    }
+    
+    
+}
+
