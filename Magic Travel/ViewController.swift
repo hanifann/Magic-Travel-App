4704dab0367b8d03c37afd97dbbdf287c0d41ff2
hanifan <hanifannurulhaq@outlook.com>
2024-02-07 20:19:47 +0700

    Initial Commit

---
+++ Magic Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic Travel
+//
+//  Created by hanifan nurul haq on 07/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
